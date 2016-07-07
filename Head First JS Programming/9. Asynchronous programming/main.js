e863b4db5e0238fd9d4612b2f8e803b295c4030b
Denis <dlmazanov@gmail.com>
2016-07-07 18:01:17 +0200

    learn handle events

---
+++ Head First JS Programming/9. Asynchronous programming/main.js
@@ -0,0 +1,31 @@
+// Handle events
+/**
+* click
+* load
+* unload
+* mousemove
+* mouseover
+* keypress
+* mouseout
+* resize
+* play
+* pause 
+* dragstart
+* drop
+* touchstart
+* touchend
+*
+**/
+
+//Dom events (page: 444):
+/**
+* taget
+* type
+* timeStamp
+* keyCode
+* clientX
+* clientY
+* touches
+**/
+
+	
