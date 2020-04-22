68f61b60dc323c99ec1fb11c6b556002f7f2cbcf
Denys Mazanov <dmazanov@mail.ru>
2020-04-22 13:08:21 +0200

    added POST request

--- Professional-JS_level-1/js-server-async/xhr-universal.js
+++ Professional-JS_level-1/js-server-async/xhr-universal.js
@@ -1,11 +1,12 @@
 const requestURL = 'https://jsonplaceholder.typicode.com/users';
 
-function sendRequest(method, url) {
+function sendRequest(method, url, body = null) {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
 
     xhr.open(method, url);
     xhr.responseType = 'json';
+    xhr.setRequestHeader('Content-Type', 'application/json');
 
     xhr.onload = () => {
       if (xhr.status >= 400) {
@@ -19,11 +20,22 @@ function sendRequest(method, url) {
       reject(xhr.response);
     }
 
-    xhr.send();
+    xhr.send(JSON.stringify(body));
   });
 }
 
 // GET request
- sendRequest('GET', requestURL)
-   .then( data => console.log(data))
-   .catch( err => console.log(err));
+// sendRequest('GET', requestURL)
+//    .then( data => console.log(data))
+//    .catch( err => console.log(err));
+
+
+// Post request
+const body = {
+  name: 'Mark',
+  age: 27
+};
+
+sendRequest('POST', requestURL, body)
+  .then( data => console.log(data))
+  .catch( err => console.log(err));
