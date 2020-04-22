427b910827321f169811577ec79389ba03d2bc62
Denys Mazanov <dmazanov@mail.ru>
2020-04-22 13:53:40 +0200

    tested Fetch API with POST method

--- Professional-JS_level-1/js-server-async/fetch.js
+++ Professional-JS_level-1/js-server-async/fetch.js
@@ -3,16 +3,31 @@
 const requestURL = 'https://jsonplaceholder.typicode.com/users';
 
 function sendRequest(method, url, body = null) {
-  return fetch(url).then( response => {
-    return response.json();
+  const headers = {
+    'Content-Type': 'application/json'
+  }
+
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(body),
+    headers: headers
+  }).then( response => {
+    if(response.ok) {
+      return response.json();
+    }
+    return response.json().then(error => {
+      const e = new Error('Something goes wrong');
+      e.data = error;
+      throw e;
+    });
   });
 
 }
 
 // GET request
-sendRequest('GET', requestURL)
-   .then( data => console.log(data))
-   .catch( err => console.log(err));
+// sendRequest('GET', requestURL)
+//    .then( data => console.log(data))
+//    .catch( err => console.log(err));
 
 
 // Post request
@@ -21,6 +36,6 @@ const body = {
   age: 27
 };
 
-// sendRequest('POST', requestURL, body)
-//   .then( data => console.log(data))
-//   .catch( err => console.log(err));
+sendRequest('POST', requestURL, body)
+  .then( data => console.log(data))
+  .catch( err => console.log(err));
