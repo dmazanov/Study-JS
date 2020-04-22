bef82a819a4d852505ec1a03010c44a3a0e20822
Denys Mazanov <dmazanov@mail.ru>
2020-04-22 12:58:45 +0200

    start create universal xhr request. create get request

---
+++ Professional-JS_level-1/js-server-async/xhr-universal.js
@@ -0,0 +1,29 @@
+const requestURL = 'https://jsonplaceholder.typicode.com/users';
+
+function sendRequest(method, url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(method, url);
+    xhr.responseType = 'json';
+
+    xhr.onload = () => {
+      if (xhr.status >= 400) {
+        reject(xhr.response);
+      } else {
+        resolve(xhr.response);
+      }
+    }
+
+    xhr.onerror = () => {
+      reject(xhr.response);
+    }
+
+    xhr.send();
+  });
+}
+
+// GET request
+ sendRequest('GET', requestURL)
+   .then( data => console.log(data))
+   .catch( err => console.log(err));
